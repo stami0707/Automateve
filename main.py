4c6c34436e93c56e3bda275732ece97c49a2e102
Süli Tamara <stami0707@gmail.com>
2022-01-07 13:00:06 +0100

    env

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 __maintainer__ = "Süli Tamara"
-__version__ = "1.1"
+__version__ = "1.2"
 __date__ = "2022.01.07."
 
 from selenium.webdriver import Chrome
@@ -9,9 +9,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
+import dotenv
+import os
 
-USER = ""  #Add meg a tevéd nevét!
-PWD = ""   #Add meg a tevéd hívójelét!
+dotenv.load_dotenv()
 
 ### OPEN BROWSER ###
 
@@ -21,10 +22,10 @@ driver.get(r"https://teveclub.hu/")
 ### LOGIN ###
 
 input_user = driver.find_element(By.NAME, 'tevenev')
-input_user.send_keys(USER)
+input_user.send_keys(os.getenv("USER"))
 
 input_pwd = driver.find_element(By.NAME, 'pass')
-input_pwd.send_keys(PWD)
+input_pwd.send_keys(os.getenv("PWD"))
 
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[src='/img_des/login_submit_46.gif'][type='image']"))).click()
 
