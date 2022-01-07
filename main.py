f33b38272aebf2c8d12bf49c195c6b2d9f91d562
Süli Tamara <stami0707@gmail.com>
2022-01-07 13:36:27 +0100

    error messages and user/pwd check

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 __maintainer__ = "Süli Tamara"
-__version__ = "1.2"
+__version__ = "1.3"
 __date__ = "2022.01.07."
 
+import os
+import tkinter
+import tkinter.messagebox
+
+import dotenv
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-import dotenv
-import os
 
 dotenv.load_dotenv()
+
+
+def messagebox(title, text):
+    root = tkinter.Tk()
+    root.withdraw()
+    tkinter.messagebox.showerror(title, text)
+    root.destroy()
+
 
 ### OPEN BROWSER ###
 
@@ -29,6 +40,10 @@ input_pwd.send_keys(os.getenv("PWD"))
 
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[src='/img_des/login_submit_46.gif'][type='image']"))).click()
 
+if driver.current_url == "https://teveclub.hu/error.pet?code=wronglogin":
+    messagebox("Automateve - Hiba", "Hibás felhasználónév vagy jelszó!")
+    quit()
+
 ### ETETÉS (1 étel és ital) ###
 
 try:
@@ -36,10 +51,11 @@ try:
 except NoSuchElementException:
     pass
 
-### TANÍTÁS (ha már van elkezdve) ###
+### TANÍTÁS ###
+
+driver.find_element(By.CSS_SELECTOR, '[alt="Tanítom a tevémet!"]').click()
 
 try:
-    driver.find_element(By.CSS_SELECTOR, '[alt="Tanítom a tevémet!"]').click()
     driver.find_element(By.NAME, "learn").click()
 except NoSuchElementException:
-    pass
+    messagebox("Automateve - Hiba", "Ma már tanult " + os.getenv("USER") + "! :( (vagy ki kell választanod, mit tanuljon mától)")
